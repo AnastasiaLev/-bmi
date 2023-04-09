f19f0c39f9d9dee92ebc1d50c104457324b9c4b2
Neto test <alevochkina96@gmail.com>
2023-04-09 13:36:13 +0700

    miles

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int bodyМass, double height) {
         double result = bodyМass / (height * height);
-        return(int) result;
+        return (int) result;
     }
 }
